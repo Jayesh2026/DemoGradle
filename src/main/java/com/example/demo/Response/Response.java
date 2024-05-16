192e313f4558421407c63f85636b42b572677ddf
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-05-16 13:11:23 +0530

    created custom ResponseEntity class

---
+++ src/main/java/com/example/demo/Response/Response.java
@@ -0,0 +1,18 @@
+package com.example.demo.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+
+    private int statusCode;
+    private String message;
+    private Object data;
+    
+}
