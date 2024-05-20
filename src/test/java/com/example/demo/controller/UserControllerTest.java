b045a733737276b5f512f460591fd9446640b080
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-05-20 11:47:33 +0530

    write user controller test cases

--- src/test/java/com/example/demo/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -11,61 +11,101 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import com.example.demo.Response.Response;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 
 @ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
-    
+
     @Mock
     UserService userServeTest;
 
     @InjectMocks
-    UserController userContrTest;
+    UserController userControlerTest;
 
     @Test
-    public void TestSaveUser(){
-        User expUser = new User(1, "Luffy", "Foosha Village");
+    public void TestSaveUser() {
+        // Given
+        User expUser = new User(1, "Luffy", "Foosha Village", "luffy@gmail.com");
         when(userServeTest.saveNewUser(expUser)).thenReturn(expUser);
-        User acutalUser = userContrTest.saveNewUser(expUser);
-        assertEquals(expUser, acutalUser);
+
+        // When
+        ResponseEntity<Response> actualResponse = userControlerTest.saveNewUser(expUser);
+
+        // Then
+        assertEquals(HttpStatus.CREATED, actualResponse.getStatusCode());
+        assertEquals("User saved successfully", actualResponse.getBody().getMessage());
+        assertEquals(expUser, actualResponse.getBody().getData());
     }
 
     @Test
-    public void testGetUserById(){
-        User expUSer = new User(101, "Zoro", "Shimotsuki Village");
-        when(userServeTest.getUserById(101)).thenReturn(expUSer);
-        User actUser = userContrTest.getUserById(101);
-        assertEquals(expUSer, actUser);
+    public void testGetUserById() {
+        // Given
+        User expUser = new User(101, "Zoro", "Shimotsuki Village", "zoro@gmail.com");
+        when(userServeTest.getUserById(101)).thenReturn(expUser);
+
+        // When
+        ResponseEntity<Response> actualResponse = userControlerTest.getUserById(101);
+
+        // Then
+        assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+        assertEquals("User retrieved successfully.", actualResponse.getBody().getMessage());
+        assertEquals(expUser, actualResponse.getBody().getData());
     }
 
     @Test
-    public void testGetAllUser(){
+    public void testGetAllUser() {
+        // Given
         List<User> userList = new ArrayList<>();
-        userList.add(new User(102, "Nami", "Cocoyasi Village"));
-        userList.add(new User(101, "Usopp", "Syrup Village"));
-        userList.add(new User(103, "Nico Robin", "Ohara"));
+        userList.add(new User(102, "Nami", "Cocoyasi Village", "nami@gmail.com"));
+        userList.add(new User(101, "Usopp", "Syrup Village", "ussop@gmail.com"));
+        userList.add(new User(103, "Nico Robin", "Ohara", "nicoRobin@gmail.com"));
 
         when(userServeTest.getAllUser()).thenReturn(userList);
-        List<User> acList = userContrTest.getAllUser();
-        assertEquals(userList, acList);
+
+        // When
+        ResponseEntity<Response> actualResponse = userControlerTest.getAllUser();
+
+        // Then
+        assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+        assertEquals("All users data retrieved successfully", actualResponse.getBody().getMessage());
+        assertEquals(userList, actualResponse.getBody().getData());
     }
 
     @Test
-    public void testUpdateUser(){
-        User expUser = new User(101, "Francky", "South Blue");
-        when(userServeTest.updateUser(101,"Francky", "South Blue")).thenReturn(expUser);
-        User acUser = userContrTest.updateUser(101, "Francky", "South Blue");
-        assertEquals(expUser, acUser);
+    public void testUpdateUser_Success() {
+        // Given
+        String name = "Francky";
+        String city = "South Blue";
+        String email = "francky@example.com";
+        User expUser = new User(101, name, city, email);
+        when(userServeTest.updateUser(name, city, email)).thenReturn(expUser);
+
+        // When
+        ResponseEntity<Response> actualResponse = userControlerTest.updateUser(name, city, email);
+
+        // Then
+        assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+        assertEquals("User updated successfully.", actualResponse.getBody().getMessage());
+        assertEquals(expUser, actualResponse.getBody().getData());
     }
 
     @Test
-    public void testDeleteUSer(){
+    public void testDeleteUser_Success() {
+        // Given
         int userIdToDelete = 101;
         doNothing().when(userServeTest).deleteUserById(userIdToDelete);
-        userContrTest.deleteUser(userIdToDelete);
-        verify(userServeTest).deleteUserById(userIdToDelete);
+
+        // When
+        ResponseEntity<Response> actualResponse = userControlerTest.deleteUser(userIdToDelete);
+
+        // Then
+        assertEquals(HttpStatus.NO_CONTENT, actualResponse.getStatusCode());
+        assertEquals("User deleted successfully.", actualResponse.getBody().getMessage());
     }
 
 }
