76e5d86eb93a88a73aea36357f0c4b4e5a16a4cb
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-05-22 12:01:27 +0530

    update database connection

--- src/main/java/com/example/demo/config/DBConnection.java
+++ src/main/java/com/example/demo/config/DBConnection.java
@@ -2,36 +2,25 @@ package com.example.demo.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class DBConnection {
-    
-    @Value("${spring.datasource.driverClassName}")
-    private String driverClassName;
 
-    @Value("${spring.datasource.url}")
-    private String url;
-
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
+    /* DataSource is an interface provided by JDBC for connecting to a database. */
 
     @Bean
     public DataSource dataSource(){
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/CustomerInfo");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("root");
 
         return dataSource;
     }
-
     
 }
--- src/test/java/com/example/demo/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -29,14 +29,11 @@ public class UserControllerTest {
 
     @Test
     public void TestSaveUser() {
-        // Given
         User expUser = new User(1, "Luffy", "Foosha Village", "luffy@gmail.com");
         when(userServeTest.saveNewUser(expUser)).thenReturn(expUser);
 
-        // When
         ResponseEntity<Response> actualResponse = userControlerTest.saveNewUser(expUser);
 
-        // Then
         assertEquals(HttpStatus.CREATED, actualResponse.getStatusCode());
         assertEquals("User saved successfully", actualResponse.getBody().getMessage());
         assertEquals(expUser, actualResponse.getBody().getData());
@@ -44,14 +41,11 @@ public class UserControllerTest {
 
     @Test
     public void testGetUserById() {
-        // Given
         User expUser = new User(101, "Zoro", "Shimotsuki Village", "zoro@gmail.com");
         when(userServeTest.getUserById(101)).thenReturn(expUser);
 
-        // When
         ResponseEntity<Response> actualResponse = userControlerTest.getUserById(101);
 
-        // Then
         assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
         assertEquals("User retrieved successfully.", actualResponse.getBody().getMessage());
         assertEquals(expUser, actualResponse.getBody().getData());
@@ -59,7 +53,6 @@ public class UserControllerTest {
 
     @Test
     public void testGetAllUser() {
-        // Given
         List<User> userList = new ArrayList<>();
         userList.add(new User(102, "Nami", "Cocoyasi Village", "nami@gmail.com"));
         userList.add(new User(101, "Usopp", "Syrup Village", "ussop@gmail.com"));
@@ -67,10 +60,8 @@ public class UserControllerTest {
 
         when(userServeTest.getAllUser()).thenReturn(userList);
 
-        // When
         ResponseEntity<Response> actualResponse = userControlerTest.getAllUser();
 
-        // Then
         assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
         assertEquals("All users data retrieved successfully", actualResponse.getBody().getMessage());
         assertEquals(userList, actualResponse.getBody().getData());
@@ -78,17 +69,14 @@ public class UserControllerTest {
 
     @Test
     public void testUpdateUser_Success() {
-        // Given
         String name = "Francky";
         String city = "South Blue";
         String email = "francky@example.com";
         User expUser = new User(101, name, city, email);
         when(userServeTest.updateUser(name, city, email)).thenReturn(expUser);
 
-        // When
         ResponseEntity<Response> actualResponse = userControlerTest.updateUser(name, city, email);
 
-        // Then
         assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
         assertEquals("User updated successfully.", actualResponse.getBody().getMessage());
         assertEquals(expUser, actualResponse.getBody().getData());
@@ -96,14 +84,11 @@ public class UserControllerTest {
 
     @Test
     public void testDeleteUser_Success() {
-        // Given
         int userIdToDelete = 101;
         doNothing().when(userServeTest).deleteUserById(userIdToDelete);
 
-        // When
         ResponseEntity<Response> actualResponse = userControlerTest.deleteUser(userIdToDelete);
 
-        // Then
         assertEquals(HttpStatus.NO_CONTENT, actualResponse.getStatusCode());
         assertEquals("User deleted successfully.", actualResponse.getBody().getMessage());
     }
